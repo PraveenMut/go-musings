3258d4e1199bca4e2a861740302cfdd1c8ccbc9c
Praveen Muthu <praveen.mut@gmail.com>
2020-07-12 19:38:53 +1000

    Initial For loops practice

---
+++ forloops/forloops.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		} else {
+			fmt.Println(n)
+		}
+	}
+
+	for i := 0; i <= 5; i += 2 {
+		fmt.Println(i)
+	}
+
+}
