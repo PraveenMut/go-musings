b8488901f0219a1aa3354f87de4a0052e6a32680
Praveen Muthu <praveen.mut@gmail.com>
2020-07-26 15:49:39 +1000

    Add initial work with switches

--- switches/switches.go
+++ switches/switches.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	var i = 2
+	fmt.Print("Write", i, " as ")
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 }
