cd35c05ab4744e3dca57a21f403b883a90dda106
Praveen Muthu <praveen.mut@gmail.com>
2020-07-26 18:21:48 +1000

    Add additional tests with arrays

--- full-representation-of-arrays/full-representation-of-arrays.go
+++ full-representation-of-arrays/full-representation-of-arrays.go
@@ -3,6 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var a [5]int
-	fmt.Println("emp:", a)
+	//  this initialises an empty array of 5 integers.
+	//  by default, null values are zeroed by 0 ints
+	var arr [5]int
+	fmt.Println("emp:", arr)
+
+	// to set a value in an array, you need to specify the index
+	// through an assignment.
+
+	arr[4] = 4
+	fmt.Println("set:", arr)
+	fmt.Println("get:", arr[4])
+
+	// calculate the length of the array
+	fmt.Println("len", len(arr))
+
+	// initialise a new array type with predef vals
+	var ary = [3]int{1, 2, 3}
+	fmt.Println("new ary print:", ary)
+
 }
