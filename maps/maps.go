91219a36056843a166cdfbf253c8cb1b1fbeb19c
praveen <praveen.mut@gmail.com>
2020-08-03 18:19:42 +1000

    Doing initial work with maps

--- maps/maps.go
+++ maps/maps.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println(" ")
+	// to create an empty map with a non-zero size,
+	// the make syntax is also used with the following:
+	// map[KEY_TYPE]VAL_TYPE, i.e. make(map[byte]int)
+
+	newTestMap := make(map[string]string)
+	fmt.Println(newTestMap)
+
+	// assigning key value pairs is like in regular language
+
+	newTestMap["key1"] = "value1"
+	newTestMap["key2"] = "value2"
+	fmt.Println(newTestMap)
+
+	// to get a key-value pair, use the standard map_name["key"]
+	findVal := newTestMap["key1"]
+	fmt.Println(findVal)
 }
