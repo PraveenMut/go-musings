7d866d34801e58dbe67c4b18b0d500166c5bccb8
praveen <praveen.mut@gmail.com>
2020-08-08 19:37:05 +1000

    Add in documentation for functions

--- functions/functions.go
+++ functions/functions.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// functions are similar to other functions in other languages
+// except that go requires explicit returns and the best practice
+// is to provide the type of the argument with <arg> <type>
+// functions require the type of return of the function
+// thus the syntax is func func_name(<arg> <type>) <output type> {}
 func plus(a int, b int) int {
 	return a + b
 }
