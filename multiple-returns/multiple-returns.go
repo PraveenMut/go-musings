1cacb1d67c6235f224100fe25ff7f09f9edff325
praveen <praveen.mut@gmail.com>
2020-08-10 18:24:54 +1000

    add in additional examples for multiple returns

--- multiple-returns/multiple-returns.go
+++ multiple-returns/multiple-returns.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func exampleOfMultipleReturns(param1, param2 int) (int, int) {
+	return param1, param2
+}
+
+func main() {
+	// to return both values
+	a, b := exampleOfMultipleReturns(1, 2)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%d and %d\n", a, b)
+
+	// to just use one var from a multiple return function, use the _ identifier
+	_, c := exampleOfMultipleReturns(2, 3)
+	fmt.Println(c)
+}
