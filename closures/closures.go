aeba2f3ee0750d8c94b7d8514bdae82940b30491
praveen <praveen.mut@gmail.com>
2020-08-23 20:36:59 +1000

    Add documentation for closures

--- closures/closures.go
+++ closures/closures.go
@@ -14,6 +14,9 @@ func closuresBaby() func() int {
 	}
 }
 
+// this another example of closures
+// where the adder func returns another func
+// whos args are of an int and return an int
 func adder() func(int) int {
 	i := 0
 	return func(x int) int {
@@ -23,17 +26,27 @@ func adder() func(int) int {
 }
 
 func main() {
+	// creating a function and the assignining it a var
 	testIterator := closuresBaby()
 
+	// as the testIterator now embodies a function
+	// it is now in round braces ()
 	fmt.Println(testIterator())
 	fmt.Println(testIterator())
 	fmt.Println(testIterator())
 	fmt.Println(testIterator())
 
+	// this is to illustrate that the state of the closed
+	// vars are purely when run. This creates a "private"
+	// namespace almost. Similar to private namespaces
+	// in java
 	newState := closuresBaby()
 	fmt.Println(newState())
 	fmt.Println(newState())
 
+	// also illustrates how you can have the exact same function
+	// with different args, but yield different outputs as it
+	// handles state independently
 	positiveSummation, negativeSummation := adder(), adder()
 	for i := 0; i < 10; i++ {
 		fmt.Println("sum is:", positiveSummation(i))
