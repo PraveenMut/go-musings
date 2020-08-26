2b2a60b41086ba02ca4ffa2e05c15c9a29d3c48f
praveen <praveen.mut@gmail.com>
2020-08-26 18:47:41 +1000

    finding fibs

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -13,6 +13,16 @@ func factorial(x int) int {
 	return x * factorial(x-1)
 }
 
+// find fibs
+func fibs(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+	return fibs(n-1) + fibs(n-2)
+}
+
 func main() {
 	// and here we call the function and assign them to vars
 	factorial1 := factorial(1)
@@ -22,4 +32,7 @@ func main() {
 	fmt.Printf("the factorial of 1 is %d\n", factorial1)
 	fmt.Printf("the factorial of 2 is %d\n", factorial2)
 	fmt.Printf("the factorial of 10 is %d\n", factorialbig)
+
+	// find fibs
+	fmt.Println(fibs(4))
 }
