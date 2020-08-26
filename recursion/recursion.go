3d777c010f842881d9de391f2ae109e7f60dbb89
praveen <praveen.mut@gmail.com>
2020-08-26 18:15:39 +1000

    Add in general documentation

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// this is an example of using recursion using golang
+// this is a simple factorial
 func factorial(x int) int {
 	if x == 0 {
 		return 1
@@ -12,9 +14,11 @@ func factorial(x int) int {
 }
 
 func main() {
+	// and here we call the function and assign them to vars
 	factorial1 := factorial(1)
 	factorial2 := factorial(2)
 	factorialbig := factorial(10)
+	// and print them here
 	fmt.Printf("the factorial of 1 is %d\n", factorial1)
 	fmt.Printf("the factorial of 2 is %d\n", factorial2)
 	fmt.Printf("the factorial of 10 is %d\n", factorialbig)
