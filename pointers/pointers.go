fa0c7e372c72bd5ce944c3b318885e922759bfdd
Praveen Muthu <praveen.mut@gmail.com>
2020-09-06 15:52:06 +1000

    Adding information about when to pass by ref or val

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -30,4 +30,18 @@ func main() {
 	fmt.Println("testValue is:", testValue)
 	fmt.Println("testValue by Pointer address is :", testValueByPointer)
 	fmt.Println("testValue by Pointer is:", *testValueByPointer)
+
+	*testValueByPointer = "newValue"
+
+	fmt.Println("the new testValue is:", testValue)
+	fmt.Println("the reference by pointer value is:", *testValueByPointer)
+
+	// deciding when to pass a pointer than sending a value is about knowing
+	// if you want to cause mutation in the passing parameter. If you wish
+	// to mutate the parameter, then defining a function with pointer args
+	// is a good approach.
+
+	// However, if you which to scope your functions and provide scoped vars
+	// then providing parameters into the function args should be done by
+	// value.
 }
