b4b95143186369c7953b57ae682c1c91872217fa
Praveen Muthu <praveen.mut@gmail.com>
2020-09-12 16:22:50 +1000

    Add initial documentation

--- structs/structs.go
+++ structs/structs.go
@@ -16,22 +16,44 @@ func newPerson(n string) *person {
 }
 
 func main() {
+	// output with a values passed in
+	// the compiler automatically the structure
+	// of the structs passed on the positionals args
 	fmt.Println(person{"Bob", 20})
 
+	// you can also explicitly define structs
 	fmt.Println(person{name: "Alice", age: 30})
 
+	// any values that are not passed in
+	// to additional/other fields are
+	// zero-valued (nulled)
 	fmt.Println(person{name: "Fred"})
 
+	// adding the & operator passes the
+	// memory address which becomes deferenced
 	fmt.Println(&person{name: "Ann", age: 40})
 
+	// this where the function passed as values
+	// works
 	fmt.Println(newPerson("Jon"))
 
+	// sperson is passed as a normal struct
+	// but then you can access individual
+	// fields of structs by using the .
+	// operator
 	sperson := person{name: "Sean", age: 50}
 	fmt.Println(sperson.name)
 
+	// you can also use pointers by using
+	// the & operator as the . operator
+	// automatically dereferences the memory address
 	sp := &sperson
 	fmt.Println(sp.age)
 
+	// you can also directly edit the pointer of
+	// the structure and edit values
+	// note that this actually changes the struct itself
+	// as it passed by reference AND not by value
 	sp.age = 41
 	fmt.Println(sp.age)
 }
