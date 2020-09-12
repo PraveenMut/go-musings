42b581512fe2e43abf043ed64975581db25fd72b
Praveen Muthu <praveen.mut@gmail.com>
2020-09-12 16:12:50 +1000

    Add in Print statements to newly created struct

--- structs/structs.go
+++ structs/structs.go
@@ -1,16 +1,37 @@
 package main
 
+import (
+	"fmt"
+)
+
 type person struct {
 	name string
 	age  int
 }
 
-func newPerson(name string) *person {
-	p := person{name: name}
+func newPerson(n string) *person {
+	p := person{name: n}
 	p.age = 42
 	return &p
 }
 
 func main() {
+	fmt.Println(person{"Bob", 20})
 
+	fmt.Println(person{name: "Alice", age: 30})
+
+	fmt.Println(person{name: "Fred"})
+
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	fmt.Println(newPerson("Jon"))
+
+	sperson := person{name: "Sean", age: 50}
+	fmt.Println(sperson.name)
+
+	sp := &sperson
+	fmt.Println(sp.age)
+
+	sp.age = 41
+	fmt.Println(sp.age)
 }
