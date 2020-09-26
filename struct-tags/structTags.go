d879ab57209f4551282f45336bed952250e206f4
praveen <praveen.mut@gmail.com>
2020-09-26 14:31:58 +1000

    Instatiate struct and print

--- struct-tags/structTags.go
+++ struct-tags/structTags.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 	"time"
 )
 
 // User struct takes name, password and preferredfish fields to create a user profile
 type User struct {
-	Name          string
+	name          string
 	password      string
 	preferredFish []string
 	createdAt     time.Time
 }
 
 func main() {
+	user1 := &User{
+		name:      "Sammy the Shark",
+		password:  "fishareawesome",
+		createdAt: time.Now(),
+	}
 
+	output, err := json.MarshalIndent(user1, "", "  ")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(output))
 }
