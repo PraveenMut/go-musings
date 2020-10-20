eddcef646729d52c23c46baee52fc506241c6810
praveen <praveen.mut@gmail.com>
2020-10-20 16:23:40 +1100

    Add Documentation to practice

--- methods/methods.go
+++ methods/methods.go
@@ -19,11 +19,18 @@ func (c circle) circumference() float64 {
 }
 
 func main() {
+	// Instantiate a struct from circle
 	circle1 := circle{radius: 10}
+	// Implement a method call on the newly instantiated circle1 struct by value.
+
 	fmt.Println("Area by Value: ", circle1.area())
+	// This will not mutate the incoming struct as it is passed by Value
 	fmt.Println("Circumference by Value: ", circle1.circumference())
 
+	// Impelement a method but in this case pass by pointer
 	circle2 := &circle1
+
+	// this has the ability to mutate the receiving struct as it passed by pointer.
 	fmt.Println("Area by Pointer Ref: ", circle2.area())
 	fmt.Println("Circumference by Pointer Ref: ", circle2.circumference())
 }
